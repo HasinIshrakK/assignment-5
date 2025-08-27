b4f0b5870b3513a51c05dcc2b1710d4ca8fb2e42
HasinIshrakK <hasinishrakk@gmail.com>
2025-08-28 00:43:29 +0600

    like managed

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,37 @@
 hearts = document.getElementById("heart");
 
+document.getElementById("like 1").addEventListener('click', function increaseHeart() {
+     hearts.innerText = parseInt(hearts.innerText) + 1 
+})
+document.getElementById("like 2").addEventListener('click', function increaseHeart() {
+     hearts.innerText = parseInt(hearts.innerText) + 1 
+})
+document.getElementById("like 3").addEventListener('click', function increaseHeart() {
+     hearts.innerText = parseInt(hearts.innerText) + 1 
+})
+document.getElementById("like 4").addEventListener('click', function increaseHeart() {
+     hearts.innerText = parseInt(hearts.innerText) + 1 
+})
+document.getElementById("like 5").addEventListener('click', function increaseHeart() {
+     hearts.innerText = parseInt(hearts.innerText) + 1 
+})
+document.getElementById("like 6").addEventListener('click', function increaseHeart() {
+     hearts.innerText = parseInt(hearts.innerText) + 1 
+})
+document.getElementById("like 7").addEventListener('click', function increaseHeart() {
+     hearts.innerText = parseInt(hearts.innerText) + 1 
+})
+document.getElementById("like 8").addEventListener('click', function increaseHeart() {
+     hearts.innerText = parseInt(hearts.innerText) + 1 
+})
+document.getElementById("like 9").addEventListener('click', function increaseHeart() {
+     hearts.innerText = parseInt(hearts.innerText) + 1 
+})
 
 
-hearts = document.getElementById("heart");
+
+coins = document.getElementById("coin");
 
 
 
-hearts = document.getElementById("heart");
+copies = document.getElementById("copy");
